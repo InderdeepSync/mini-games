7cb38ac9474a8399e602eaf75533531c64d7e3f2
Inderdeep Singh <buntycool1998@gmail.com>
2023-06-11 17:56:04 -0400

    Add Color Matching game

---
+++ src/color-match/index.jsx
@@ -0,0 +1,82 @@
+// COLOR MATCH - Final Code
+import React from 'react'
+import _ from 'lodash'
+
+import styles from './styles.module.css'
+const colors = ['black', 'blue', 'red', 'green', 'yellow'];
+
+const randomColors = () => {
+    const meaningWord = _.sample(colors);
+    const inkWord = _.sample(colors);
+    const inkColor = Math.random() < 0.4 ? meaningWord : _.sample(colors);
+    return {
+        meaningWord,
+        inkWord,
+        inkColor,
+        meaningInkMatch: meaningWord === inkColor,
+    };
+};
+
+export default class ColorGame extends React.Component {
+    state = {
+        gameStatus: 'playing', // Possible other values: correct, wrong
+    };
+
+    colorValues = randomColors();
+
+    handleClick = yesClick => {
+        this.setState(prevState => {
+            if (prevState.gameStatus !== 'playing') {
+                return null; // Do nothing.
+            }
+            const correctClick =
+                (this.colorValues.meaningInkMatch ^ yesClick) === 0;
+            return {
+                gameStatus: correctClick ? 'correct' : 'wrong',
+            };
+        }, this.resetGameAfterDelay);
+    };
+
+    resetGameAfterDelay = () => {
+        setTimeout(() => {
+            this.colorValues = randomColors();
+            this.setState({ gameStatus: 'playing' });
+        }, 500);
+    };
+
+    render() {
+        const {
+            meaningWord,
+            inkWord,
+            inkColor,
+        } = this.colorValues;
+        const { gameStatus } = this.state;
+        return (
+            <div className={styles.game}>
+                <div className={styles.help}>
+                    Does the meaning of the top word match the ink
+                    color of the bottom word?
+                </div>
+                <div className={styles.body}>
+                    <div
+                        className={`${styles["game-status"]} ${styles[`status-${gameStatus}`]}`}
+                    />
+                    <div className={styles.meaning}>
+                        {meaningWord.toUpperCase()}
+                    </div>
+                    <div className={styles.ink} style={{ color: inkColor }}>
+                        {inkWord.toUpperCase()}
+                    </div>
+                    <div className={styles.buttons}>
+                        <button onClick={() => this.handleClick(true)}>
+                            YES
+                        </button>
+                        <button onClick={() => this.handleClick(false)}>
+                            NO
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import {createRoot} from "react-dom/client";
+import StarGame from "./star-matching/index.jsx";
+import ColorGame from "./color-match/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import GameGenerator from "./memory-challenge/index.jsx";
+import TargetSum from "./target-sum/index.jsx";
+
+const router = createBrowserRouter([
+    // {
+    //     path: "/star-matching",
+    //     element: <StarGame/>,
+    // },
+    {
+        path: "/color-matching",
+        element:<ColorGame/>
+    },
+    // {
+    //     path: "/memory-challenge",
+    //     element: <GameGenerator/>
+    // },
+    // {
+    //     path: "/target-sum",
+    //     element: <TargetSum/>
+    // }
+]);
+
+createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
